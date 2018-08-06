6af635565fa293d1c04a8ecfbbbe620e512ab2f2
Shliahtur <shliahtur@me.com>
2018-08-06 17:22:34 +0300

    add app

---
+++ app/app.jsx
@@ -0,0 +1,8 @@
+var ReactDOM = require("react-dom");
+var React = require("react");
+var PhonesList = require("./phoneslist.jsx");
+  
+ReactDOM.render(
+    <PhonesList />,
+    document.getElementById("container")
+)
